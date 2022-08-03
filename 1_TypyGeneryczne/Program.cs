f41d4a095581715c67b9d1e6e47dc043844daf9d
Tomasz Mazurek <mazurek.tomasz97@gmail.com>
2022-08-03 22:02:28 +0200

    1_4KolejkaKolowa

--- 1_TypyGeneryczne/Program.cs
+++ 1_TypyGeneryczne/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _1_TypyGeneryczne
 {
@@ -10,7 +6,27 @@ namespace _1_TypyGeneryczne
     {
         static void Main(string[] args)
         {
+            var kolejka = new KolejkaKolowa(pojemnosc: 3);
 
+            while (true)
+            {
+                var wartosc = 0.0;
+                var wartoscwejsciowa = Console.ReadLine();
+
+                if (double.TryParse(wartoscwejsciowa, out wartosc))
+                {
+                    kolejka.Zapisz(wartosc);
+                    continue;
+                }
+                break;
+            }
+
+            Console.WriteLine("W naszej kolejce jest : ");
+
+            while (!kolejka.JestPusty)
+            {
+                Console.WriteLine("\t\t" + kolejka.Czytaj());
+            }
         }
     }
 }
