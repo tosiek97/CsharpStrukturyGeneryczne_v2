fb6ddfb34ace5d9f1ee14d94f6c92c4b45842519
Tomasz Mazurek <mazurek.tomasz97@gmail.com>
2022-08-04 20:53:51 +0200

    1_5 UnitTests

---
+++ 1_TypyGeneryczneTests/KolejkaKolowaTests.cs
@@ -0,0 +1,62 @@
+﻿using _1_TypyGeneryczne;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace _1_TypyGeneryczneTests
+{
+    [TestClass]
+    public class KolejkaKolowaTests
+    {
+        [TestMethod]
+        public void NowaKolejkaJestPusta()
+        {
+            var kolejka = new KolejkaKolowa();
+
+            Assert.IsTrue(kolejka.JestPusty);
+        }
+
+        [TestMethod]
+        public void KolejkaTrzyElementowaJestPelnaPoTrzechZapisach()
+        {
+            var kolejka = new KolejkaKolowa(pojemnosc: 3);
+            kolejka.Zapisz(3.8);
+            kolejka.Zapisz(5.7);
+            kolejka.Zapisz(9.3);
+
+            Assert.IsTrue(kolejka.JestPelny);
+        }
+
+        [TestMethod]
+        public void PierwszyWchodziPierwszyWychodzi()
+        {
+            var kolejka = new KolejkaKolowa(pojemnosc: 3);
+            var wartosc1 = 4.6;
+            var wartosc2 = 3.7;
+
+            kolejka.Zapisz(wartosc1);
+            kolejka.Zapisz(wartosc2);
+
+            Assert.AreEqual(wartosc1, kolejka.Czytaj());
+            Assert.AreEqual(wartosc2, kolejka.Czytaj());
+            Assert.IsTrue(kolejka.JestPusty);
+        }
+
+        [TestMethod]
+        public void NadisujeGdyJestWiekszaNizPojemnosc()
+        {
+            var kolejka = new KolejkaKolowa(pojemnosc: 3);
+            var wartosci = new[] { 1.2, 2.4, 3.6, 4.8, 5.3, 6.8 };
+
+            foreach (var wartosc in wartosci)
+            {
+                kolejka.Zapisz(wartosc);
+            }
+
+            Assert.IsTrue(kolejka.JestPelny);
+            Assert.AreEqual(wartosci[3], kolejka.Czytaj());
+            Assert.AreEqual(wartosci[4], kolejka.Czytaj());
+            Assert.AreEqual(wartosci[5], kolejka.Czytaj());
+            Assert.IsTrue(kolejka.JestPusty);
+        }
+    }
+}
---
+++ 1_TypyGeneryczneTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("1_TypyGeneryczneTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("1_TypyGeneryczneTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7af1f6fe-47e6-4766-97db-4037c97d92d4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
